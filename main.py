fa81256fed43554b1e9e34cc851f9421d3e08fa1
ArthurFerreira2 <arthur.ferreira2@gmail.com>
2020-11-03 07:28:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 def hello(): 
 	return "welcome to the Oracle web app with python in DevSecOPs !!!"
 
+@app.route('/oracle') 
+def hello(): 
+	return "welcome to the Oracle containerization process !!!"
 
 if __name__ == "__main__": 
 	app.run(host ='0.0.0.0', port = 5000, debug = True) 
